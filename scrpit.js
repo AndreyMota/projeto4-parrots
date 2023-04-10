77874207785b3b21e208dcfc52342ac6cbd09e4c
AndreyMota <andreymotam1@gmail.com>
2023-04-10 17:33:31 -0300

    teoricamente tudo pronto

--- scrpit.js
+++ scrpit.js
@@ -1,4 +1,6 @@
 let qntdCarta;
+let jogadas = 0;
+let cartaCerta = 0;
 
 while (true) {
     qntdCarta = prompt('Quantidade de cartas');
@@ -6,6 +8,9 @@ while (true) {
         break;
     }
 }
+
+qntdCarta = Number(qntdCarta);
+
 
 
 let papagaios = ['bobrossparrot.gif', 'explodyparrot.gif', 'fiestaparrot.gif', 'metalparrot.gif', 'revertitparrot.gif', 'tripletsparrot.gif', 'unicornparrot.gif'];
@@ -24,13 +29,13 @@ function comparador() {
 	return Math.random() - 0.5; 
 }
 
-console.log(papaga)
+////console.log(papaga)
 
 const cartas = document.querySelector('.cartas');
 cartas.innerHTML = '';
 for (i=0; i<papaga.length;i++){
-    console.log(i);
-    cartas.innerHTML += `<div class="papa" id="${i}" onclick="viraCarta(this)"><div class="back face"><img src="./images/back.png" alt=""></div><div class="front face"><img class="carta" src="./images/${papaga[i]}" alt=""></div></div>`;
+    //console.log(i);
+    cartas.innerHTML += `<div data-test="card" class="papa" id="${i}" onclick="viraCarta(this)"><div class="back face"><img data-test="face-down-image" src="./images/back.png" alt=""></div><div class="front face"><img data-test="face-up-image" class="carta" src="./images/${papaga[i]}" alt=""></div></div>`;
 }
 
 
@@ -43,12 +48,13 @@ let idc1 = null;
 let idc2 = null;
 
 function viraCarta(carta) {
+    //acabou();
     if (carta1 != null && carta2 != null) {
-        console.log('É indefinido poq ja tem 2 cartas em jogo');
+        //console.log('É indefinido poq ja tem 2 cartas em jogo');
         return undefined;
     }
 
-    console.log('oxi inicial');
+    //console.log('oxi inicial');
 
     //ID IDENTIFICADOR DE CADA CARTA
     let aid = carta.getAttribute('id')
@@ -58,8 +64,8 @@ function viraCarta(carta) {
     else if (idc2 === null) {
         idc2 = aid;
     }
-    console.log(`aid c1: ${idc1}`);
-    console.log(`aid c2: ${idc2}`);
+    //console.log(`aid c1: ${idc1}`);
+    //console.log(`aid c2: ${idc2}`);
 
     if (idc1 == idc2) {
         idc2 = null;
@@ -74,15 +80,19 @@ function viraCarta(carta) {
     if (carta1 === null && carta2 == null) {
         carta1 = carta;
         carta.classList.add('flip');
+        jogadas ++;
     }
     else {
         carta2 = carta;
         carta2.classList.add('flip');
+        jogadas ++;
         
     }
     setTimeout(verificaPar, 1500, carta1, carta2);
+
     //if (carta != null)
-    console.log('oxi');
+    //console.log('oxi');
+
     
 }
 
@@ -95,9 +105,11 @@ function verificaPar(c1=null, c2=null) {
     gmi1 = gmi1.getAttribute('src');
     let gmi2 = c2.lastElementChild.firstElementChild;
     gmi2 = gmi2.getAttribute('src');
-    console.log(gmi1);
-    console.log(gmi2);
+    //console.log(gmi1);
+    //console.log(gmi2);
     if (gmi1 === gmi2) {
+        cartaCerta ++;
+        cartaCerta ++;
         carta1.removeAttribute('onclick');
         carta2.removeAttribute('onclick');
         carta1 = null;
@@ -105,6 +117,12 @@ function verificaPar(c1=null, c2=null) {
         idc1 = null;
         idc2 = null;
         //return alert('É par!!');
+        //alert(`${qntdCarta}, ${cartaCerta}`);
+        //console.log('acabou');
+        if (cartaCerta === qntdCarta) {
+            //console.log('RETORNA TRUE');
+            alert(`Você ganhou em ${jogadas} jogadas!`)
+        }
     }
 
     if (gmi1 != gmi2) {
